769d1c36fcafe24584171aedd2dad704af3333c2
Jeff Fowler <jf@onomondo.com>
2024-02-28 10:49:57 +0100

    split darwin

--- syscall/darwin.c
+++ syscall/darwin.c
@@ -10,12 +10,14 @@ static inline int64_t syscall_6(int64_t num, int64_t arg1, int64_t arg2, int64_t
     int64_t flags;
 
     __asm__ __volatile__ (
-        "mov x8, %2;\n"               // Move syscall number into x8
-        "svc #0;\n"                   // Perform system call
-        "mov %1, x11;\n"              // Assuming we want to preserve a specific register's state post-syscall
-        : "=r" (result), "=r" (flags) // Outputs
-        : "r" (num)                   // Inputs
-        : "x8", "x11"                 // Clobber list
+        "movq %6, %%r10;\n"
+        "movq %7, %%r8;\n"
+        "movq %8, %%r9;\n"
+        "syscall;\n"
+        "movq %%r11, %1;\n"
+        : "=a" (result), "=r" (flags)
+        : "a" (num), "D" (arg1), "S" (arg2), "d" (arg3), "r" (arg4), "r" (arg5), "r" (arg6)
+        : "%r10", "%r8", "%r9", "%rcx", "%r11"
     );
 
     RETURN_SYSCALL_RESULT(result, flags);
@@ -26,12 +28,13 @@ static inline int64_t syscall_5(int64_t num, int64_t arg1, int64_t arg2, int64_t
     int64_t flags;
 
     __asm__ __volatile__ (
-        "mov x8, %2;\n"               // Move syscall number into x8
-        "svc #0;\n"                   // Perform system call
-        "mov %1, x11;\n"              // Assuming we want to preserve a specific register's state post-syscall
-        : "=r" (result), "=r" (flags) // Outputs
-        : "r" (num)                   // Inputs
-        : "x8", "x11"                 // Clobber list
+        "movq %6, %%r10;\n"
+        "movq %7, %%r8;\n"
+        "syscall;\n"
+        "movq %%r11, %1;\n"
+        : "=a" (result), "=r" (flags)
+        : "a" (num), "D" (arg1), "S" (arg2), "d" (arg3), "r" (arg4), "r" (arg5)
+        : "%r10", "%r8", "%rcx", "%r11"
     );
 
     RETURN_SYSCALL_RESULT(result, flags);
@@ -42,12 +45,12 @@ static inline int64_t syscall_4(int64_t num, int64_t arg1, int64_t arg2, int64_t
     int64_t flags;
 
     __asm__ __volatile__ (
-        "mov x8, %2;\n"               // Move syscall number into x8
-        "svc #0;\n"                   // Perform system call
-        "mov %1, x11;\n"              // Assuming we want to preserve a specific register's state post-syscall
-        : "=r" (result), "=r" (flags) // Outputs
-        : "r" (num)                   // Inputs
-        : "x8", "x11"                 // Clobber list
+        "movq %6, %%r10;\n"
+        "syscall;\n"
+        "movq %%r11, %1;\n"
+        : "=a" (result), "=r" (flags)
+        : "a" (num), "D" (arg1), "S" (arg2), "d" (arg3), "r" (arg4)
+        : "%r10", "%rcx", "%r11"
     );
 
     RETURN_SYSCALL_RESULT(result, flags);
@@ -58,15 +61,11 @@ static inline int64_t syscall_3(int64_t num, int64_t arg1, int64_t arg2, int64_t
     int64_t flags;
 
     __asm__ __volatile__ (
-        "mov x0, %2;\n"               // Move first syscall argument into x0
-        "mov x1, %3;\n"               // Move second syscall argument into x1
-        "mov x2, %4;\n"               // Move second syscall argument into x1
-        "mov x8, %5;\n"               // Move syscall number into x8
-        "svc #0;\n"                   // Perform system call
-        "mov %1, x11;\n"              // Assuming we want to preserve a specific register's state post-syscall
-        : "=r" (result), "=r" (flags) // Outputs
-        : "r" (arg1), "r" (arg2), "r" (arg3), "r" (num) // Inputs
-        : "x0", "x1", "x2", "x8", "x11"    // Clobber list
+        "syscall;\n"
+        "movq %%r11, %1;\n"
+        : "=a" (result), "=r" (flags)
+        : "a" (num), "D" (arg1), "S" (arg2), "d" (arg3)
+        : "%rcx", "%r11"
     );
 
     RETURN_SYSCALL_RESULT(result, flags);
@@ -77,14 +76,11 @@ static inline int64_t syscall_2(int64_t num, int64_t arg1, int64_t arg2) {
     int64_t flags;
 
     __asm__ __volatile__ (
-        "mov x0, %2;\n"               // Move first syscall argument into x0
-        "mov x1, %3;\n"               // Move second syscall argument into x1
-        "mov x8, %4;\n"               // Move syscall number into x8
-        "svc #0;\n"                   // Perform system call
-        "mov %1, x11;\n"              // Assuming we want to preserve a specific register's state post-syscall
-        : "=r" (result), "=r" (flags) // Outputs
-        : "r" (arg1), "r" (arg2), "r" (num) // Inputs
-        : "x0", "x1", "x8", "x11"    // Clobber list
+        "syscall;\n"
+        "movq %%r11, %1;\n"
+        : "=a" (result), "=r" (flags)
+        : "a" (num), "D" (arg1), "S" (arg2)
+        : "%rcx", "%r11"
     );
 
     RETURN_SYSCALL_RESULT(result, flags);
@@ -95,13 +91,11 @@ static inline int64_t syscall_1(int64_t num, int64_t arg1) {
     int64_t flags;
 
     __asm__ __volatile__ (
-        "mov x0, %3;\n"               // Move first syscall argument into x0
-        "mov x8, %2;\n"               // Move syscall number into x8
-        "svc #0;\n"                   // Perform system call
-        "mov %1, x11;\n"              // Assuming we want to preserve a specific register's state post-syscall
-        : "=r" (result), "=r" (flags) // Outputs
-        : "r" (num), "r" (arg1)       // Inputs
-        : "x0", "x8", "x11"           // Clobber list
+        "syscall;\n"
+        "movq %%r11, %1;\n"
+        : "=a" (result), "=r" (flags)
+        : "a" (num), "D" (arg1)
+        : "%rcx", "%r11"
     );
 
     RETURN_SYSCALL_RESULT(result, flags);
@@ -112,12 +106,11 @@ static inline int64_t syscall_0(int64_t num) {
     int64_t flags;
 
     __asm__ __volatile__ (
-        "mov x8, %2;\n"               // Move syscall number into x8
-        "svc #0;\n"                   // Perform system call
-        "mov %1, x11;\n"              // Assuming we want to preserve a specific register's state post-syscall
-        : "=r" (result), "=r" (flags) // Outputs
-        : "r" (num)                   // Inputs
-        : "x8", "x11"                 // Clobber list
+        "syscall;\n"
+        "movq %%r11, %1;\n"
+        : "=a" (result), "=r" (flags)
+        : "a" (num)
+        : "%rcx", "%r11"
     );
 
     RETURN_SYSCALL_RESULT(result, flags);
